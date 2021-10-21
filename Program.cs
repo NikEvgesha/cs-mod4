fa796341246bb724791b8610fbd80c7e2ab5dc87
Evgesha Nik <nikitenkowa.e@yandex.ru>
2021-10-21 11:19:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace cs_mod4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свой любимый цвет на английском с маленькой буквы: ");
+            var mycolor = Console.ReadLine();
+
+            if (mycolor == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (mycolor == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is green!");
+            }
+
+            else if (mycolor == "yellow")
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Your color is yellow!");
+            }
+
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("I don't know this color! My favourite color is cyan!");
+            }
+        }
+    }
+}
